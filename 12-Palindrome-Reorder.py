c188ae5c5924b961a107e13e3b7b83bb566e804e
farif1234 <farif1234@gmail.com>
2025-03-08 13:19:25 -0800

    solved problem 12

--- 12-Palindrome-Reorder.py
+++ 12-Palindrome-Reorder.py
@@ -11,7 +11,41 @@ Example
 Input:
 AAAACACBA
 
+SQAADSDTQ
+ADQSTSQDA
+
+
 Output:
 AACABACAA
 """
 s = input()
+
+count = [0] * 26
+
+for char in s:
+    count[ord(char) - ord('A')] += 1
+
+numOfOdds = 0
+for i, c in enumerate(count):
+    if c % 2 != 0:
+        numOfOdds += 1
+
+if (len(s) % 2 == 0 and numOfOdds > 0) or (numOfOdds > 1):
+    print("NO SOLUTION")
+else:
+    res = [''] * len(s)
+    resI = 0
+    for i, c in enumerate(count):
+        letter = chr(i + ord('A'))
+        if c % 2 == 0:
+            for _ in range(c // 2):
+                res[resI] = letter
+                res[len(res) - 1 - resI] = letter
+                resI += 1
+        else:
+            mid = len(s) // 2
+
+            res[mid - c // 2 : mid + 1 + c // 2] = [letter] * c
+
+
+    print(''.join(res))
